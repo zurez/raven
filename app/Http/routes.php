7261665874bcc30888f9cd6315eb8f962b2c21bc
zurez <zurez4u@gmail.com>
2016-03-05 22:33:39 +0530

    route added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,8 +10,10 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-// Route::get('/','PageController@dashboard');
+Route::get('/',function(){
+	return "please go to <a href='general'>Click Here</a>";
+});
+// Route::get('/','PageController@dashboard');;
 Route::get('general','PageController@general');
 Route::post('general','AssetController@store_asset');
 Route::get('all/assets',array('as'=>'all_asset','uses'=>'AssetController@all_asset'));
