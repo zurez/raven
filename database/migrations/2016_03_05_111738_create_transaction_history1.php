a52480d8185730189717bb2af56bb9775faa1c90
zurez <zurez4u@gmail.com>
2016-04-14 22:39:30 +0530

    migration

---
+++ database/migrations/2016_03_05_111738_create_transaction_history1.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionHistory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('asset_id');
+            $table->string('asset_tag');
+            $table->date('date');
+            $table->string('type');
+            $table->string('action');
+            $table->string('notes');
+            $table->string('costs');
+            $table->string('creator');
+            $table->string('last_updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('transactions');
+    }
+}
