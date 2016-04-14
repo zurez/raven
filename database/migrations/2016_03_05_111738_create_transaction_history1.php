91f32f27a134352c5943116f5fbdeded00683463
zurez <zurez4u@gmail.com>
2016-04-14 22:40:20 +0530

    migration

--- database/migrations/2016_03_05_111738_create_transaction_history1.php
+++ database/migrations/2016_03_05_111738_create_transaction_history1.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTransactionHistory extends Migration
+class CreateTransactionHistory1 extends Migration
 {
     /**
      * Run the migrations.
